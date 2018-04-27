bf19a4918e447e8e947c80ba84225cba2d31a122
John McDowell <johnmcdowell0801@gmail.com>
2018-04-27 14:07:18 +0000

    third room

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
+// IMPLEMENT TESTING ON USERINPUT
+
 var logArray = [];
 var rooms = [];
 
 var openingAudio = new Audio("audio/grandfather_clock.wav");
 openingAudio.load();
-
-
+var lightSwitchOnOff = new Audio("audio/light_switch_on_off.wav");
+lightSwitchOnOff.load();
+var laugh_1 = new Audio("audio/laugh_1.wav");
+laugh_1.load();
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
@@ -16,8 +20,10 @@ function sleep(ms) {
 // PLAY AUDIO =====================================================================================
 
 function playAudio() {
-    currentRoom.audio.play();
-    console.log("playing " + currentRoom.audioName)
+    if (currentRoom.playAudio == true) {
+        currentRoom.audio.play();
+        console.log("playing " + currentRoom.audioName)
+    }
 }
 
 // ================================================================================================
@@ -27,11 +33,11 @@ function playAudio() {
 // INPUT VISIBLE ==================================================================================
 
 function inputVisible(visibility) {
-    if(visibility == false){
-        document.getElementById("userInput").style.visibility='hidden';
+    if (visibility == false) {
+        document.getElementById("userInput").style.visibility = 'hidden';
     }
-    if(visibility == true){
-        document.getElementById("userInput").style.visibility='visible';
+    if (visibility == true) {
+        document.getElementById("userInput").style.visibility = 'visible';
         document.getElementById("userInput").focus();
     }
 }
@@ -64,21 +70,39 @@ function printText(toPrint) {
 // ================================================================================================
 
 
+
+
+
 // PRINTROOMINFO ===================================================================================
 
 async function printRoomInfo() {
-        
-        var exitsCombined = [];
-        for(var i = 0; i < currentRoom.exits.length; i++) {
-            exitsCombined[i] = currentRoom.exits[i];
-        }
-        inputVisible(currentRoom.inputVisible);
-        printText(currentRoom.status + "\n" + exitsCombined.join("\n"));
-        
-        await sleep(currentRoom.delayTime);
-        inputVisible(currentRoom.inputVisibleAfterDelay);
-        printText(currentRoom.delayedDescription);
+
+    var exitsCombined = [];
+    for (var i = 0; i < currentRoom.exits.length; i++) {
+        exitsCombined[i] = currentRoom.exits[i];
     }
+    inputVisible(currentRoom.inputVisible);
+    printText(currentRoom.status + "\n" + exitsCombined.join("\n"));
+
+    await sleep(currentRoom.delayTime);
+    console.log(currentRoom.audioName + " finished")
+    inputVisible(currentRoom.inputVisibleAfterDelay);
+    printText(currentRoom.delayedDescription);
+}
+
+// ================================================================================================
+
+
+
+
+// PARSER ========================================================================================
+
+function parser(userInput) {
+
+    userInput = userInput.replace("8", "eight");
+    return userInput;
+
+}
 
 // ================================================================================================
 
@@ -87,24 +111,32 @@ async function printRoomInfo() {
 
 function evaluateUserInput() {
 
+    var userInput = document.getElementById("userInput").value.toLowerCase();
+
     // check that user has inputted something
-    if (document.getElementById("userInput").value != "") {
-        
-        // look through exitStrings of current room
-        for(var i = 0; i < currentRoom.exitStrings.length; i++) {
-            
-            // if the input matches the current room exitString change rooms and print new room description
-            if (document.getElementById("userInput").value.toLowerCase() == currentRoom.exitStrings[i]) {
-            console.log("user input evaluated");
-            console.log(i);
-            currentRoom = changeRoom(i);
-            console.log("Current Room: " + currentRoom.name);
-            printText(printRoomInfo());
-            return;
+    if (userInput != "") {
+
+        //parse userInput
+        userInput = parser(userInput);
+        console.log("userInput parsed: " + userInput);
+
+        // split userInput into words
+        var userInputWords = userInput.split(" ");
+
+
+        // iterate through words in userInput
+        for (var i = 0; i < userInputWords.length; i++)
+            // if the word is a key of the keyRoomDict
+            if (currentRoom.keyRoomDict[userInputWords[i]]) {
+                console.log(currentRoom.keyRoomDict[userInputWords[i]]);
+                logArray = [];
+                currentRoom = rooms[currentRoom.keyRoomDict[userInputWords[i]]];
+                console.log("Current Room: " + currentRoom.name);
+                printText(printRoomInfo());
+                return;
             }
-        }
-        
-        // return message when userInput does not match keyString
+
+        // return message when userInput does not contain keyString
         printText("Nothing happens...");
     }
 }
@@ -115,49 +147,31 @@ function evaluateUserInput() {
 
 
 
-// CHANGE ROOM ===================================================================================
-
-function changeRoom(roomIndex) {
-        return rooms[currentRoom.adjacentRooms[roomIndex]];
-    }
-
-// ================================================================================================
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // ROOM OBJECT =====================================================================================
 
-let Room = function(name, status, exits, exitStrings, adjacentRooms, delayTime, delayedDescription, inputVisible, inputVisibleAfterDelay, audio, audioName) {
-  this.name = name;
-  this.status = status;
-  this.exits = exits;
-  this.exitStrings = exitStrings;
-  this.adjacentRooms = adjacentRooms;
-  this.delayTime = delayTime;
-  this.delayedDescription = delayedDescription;
-  this.inputVisible = inputVisible;
-  this.inputVisibleAfterDelay = inputVisibleAfterDelay;
-  this.audio = audio;
-  this.audioName = audioName;
-  
-}
+let Room = function(name, status, exits, keyRoomDict, delayTime, delayedDescription, inputVisible, inputVisibleAfterDelay, audio, audioName, playAudio) {
+    this.name = name;
+    this.status = status;
+    this.exits = exits;
+    this.keyRoomDict = keyRoomDict;
+    this.delayTime = delayTime;
+    this.delayedDescription = delayedDescription;
+    this.inputVisible = inputVisible;
+    this.inputVisibleAfterDelay = inputVisibleAfterDelay;
+    this.audio = audio;
+    this.audioName = audioName;
+    this.playAudio = playAudio;
 
-rooms[0] = new Room("firstRoom", "It is pitch black.", [], ["8"], [1], 45000, "Do you have the time?", false, true, openingAudio, "opening audio");
+};
+
+rooms["bellsRoom"] = new Room("bellsRoom", "It is pitch dark.", [], { eight: "seeLightRoom" }, 45000, "Do you have the time?", false, true, openingAudio, "opening audio", true);
+
+rooms["seeLightRoom"] = new Room("seeLightRoom", "You see a sliver of light peeking through on the ground in front of you. \nIt must be from under a door. \nIs there someone out there?", [""], { door: "firstRoomNorth" }, 0, "", true, true, lightSwitchOnOff, "lightSwitchOnOff", true);
+
+rooms["firstRoomNorth"] = new Room("firstRoomNorth", "The door is locked. \nMaybe theres something in you could use to open the door? \nTry looking around the room (north, south, east, west)", [""], { south: "bellsRoom" }, 10000, "Theres someone outside the door...", true, true, laugh_1, "laugh_1", true);
 
 // ================================================================================================
 
-var currentRoom = rooms[0];
-
+var currentRoom = rooms["bellsRoom"];
