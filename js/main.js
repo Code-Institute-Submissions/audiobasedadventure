817da3e0882698b29e064e58c07793bde43b7f3a
John McDowell <johnmcdowell0801@gmail.com>
2018-04-30 11:24:08 +0000

    refined main script a little

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 // IMPLEMENT TESTING ON USERINPUT
+// make delayed description occur on audio end
+// fix scroll bar issue
+// get user input once
+// use jquery
+// build inventory
+// implement verbs
+// create item class
 
 var logArray = [];
+var logHistoryArray = [];
 var rooms = [];
 
 var openingAudio = new Audio("audio/grandfather_clock.wav");
@@ -10,24 +18,88 @@ lightSwitchOnOff.load();
 var laugh_1 = new Audio("audio/laugh_1.wav");
 laugh_1.load();
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 
+// PROCESS USER INPUT ============================================================================
 
+function processUserInput() {
 
-// PLAY AUDIO =====================================================================================
+    // userInput is made into lower case
+    var userInput = document.getElementById("userInput").value.toLowerCase();
 
-function playAudio() {
-    if (currentRoom.playAudio == true) {
-        currentRoom.audio.play();
-        console.log("playing " + currentRoom.audioName)
+    // check that user has inputted something
+    if (userInput != "") {
+
+        // print userInput in console
+        printToLog(userInput);
+
+        // parse userInput
+        userInput = parser(userInput);
+        console.log("userInput parsed: " + userInput);
+
+        // split userInput into words
+        var userInputWords = userInput.split(" ");
+
+        // iterate through words in userInput
+        for (var i = 0; i < userInputWords.length; i++)
+            // if the word is a key of the keyRoomDict
+            if (currentRoom.keyRoomDict[userInputWords[i]]) {
+                console.log(currentRoom.keyRoomDict[userInputWords[i]]);
+
+                // add the current log to the logHistoryArray
+                logHistoryArray = logHistoryArray.concat(logArray);
+                // empty the log
+                logArray = [];
+
+                // if the keyValue is a word
+                if (isNaN(currentRoom.keyRoomDict[userInputWords[i]] * 1)) {
+                    // change currentRoom
+                    currentRoom = rooms[currentRoom.keyRoomDict[userInputWords[i]]];
+                    console.log("Current Room: " + currentRoom.name);
+                    // process room attributes
+                    processRoomAttributes();
+                    // print new room description to log
+                    printToLog(currentRoom.descprition[currentRoom.descriptionIndex]);
+                    // play new room audio
+                    playAudio();
+                    return;
+                }
+                // if the keyValue is a number
+                else {
+                    currentRoom.descriptionIndex = currentRoom.keyRoomDict[userInputWords[i]];
+                    currentRoom.playAudio = false;
+                    currentRoom.displayDelayedDescription = false;
+                    printToLog(currentRoom.description);
+                    return;
+                }
+            }
+        // return message when userInput does not contain keyString
+        printToLog("Nothing happens...");
     }
+    else {
+        console.log("Empty userInput")
+    }
+}
+// ================================================================================================
+
+// PARSER ========================================================================================
+
+function parser(userInput) {
+    userInput = userInput.replace("8", "eight");
+    return userInput;
 }
 
 // ================================================================================================
 
+
+// PROCESS ROOM ATTRIBUTES ========================================================================
+
+function processRoomAttributes() {
+
+    // check the inputVisible attribute of the current room
+    inputVisible(currentRoom.inputVisible);
+}
+// ================================================================================================
 
 
 // INPUT VISIBLE ==================================================================================
@@ -41,26 +113,18 @@ function inputVisible(visibility) {
         document.getElementById("userInput").focus();
     }
 }
-
 // ================================================================================================
 
 
+// PRINT TO LOG =====================================================================================
 
-// PRINT TEXT =====================================================================================
+function printToLog(textToPrint) {
 
-function printText(toPrint) {
-    // condition to confirm valid userInput
-    if (toPrint == null) {
-        toPrint = document.getElementById("userInput").value;
-        if (toPrint == "") {
-            document.getElementById("console").innerHTML += "<br>" + "Invalid user input";
-        }
-    }
     // add input to log
-    console.log(toPrint);
-    logArray.push(toPrint);
+    logArray.push(textToPrint);
     // print log
     document.getElementById("console").innerHTML = logArray.join("\n");
+
     // keep scroll bar at bottom
     var textarea = document.getElementById("console");
     if (textarea != null) {
@@ -71,91 +135,46 @@ function printText(toPrint) {
 
 
 
+// AUDIO =====================================================================================
 
-
-// PRINTROOMINFO ===================================================================================
-
-async function printRoomInfo() {
-
-    var exitsCombined = [];
-    for (var i = 0; i < currentRoom.exits.length; i++) {
-        exitsCombined[i] = currentRoom.exits[i];
+function playAudio() {
+    if (currentRoom.playAudio == true) {
+        currentRoom.audio.play();
+        console.log("playing " + currentRoom.audioName)
     }
-    inputVisible(currentRoom.inputVisible);
-    printText(currentRoom.status[currentRoom.statusIndex] + "\n" + exitsCombined.join("\n"));
+    // runs audioEndedFunction when audio has ended
+    currentRoom.audio.addEventListener('ended', function() {
+        console.log(currentRoom.audioName + " ended")
+        // print last description in array
+        printToLog(currentRoom.descriptions[currentRoom.descriptions.length-1])
+    })
+}
 
-    await sleep(currentRoom.delayTime);
+// ================================================================================================
+
+
+// AUDIO ON END =====================================================================================
+
+function audioEnded() {
     console.log(currentRoom.audioName + " finished")
     inputVisible(currentRoom.inputVisibleAfterDelay);
-    if(currentRoom.displayDelayedDescription){
-        printText(currentRoom.delayedDescription);
-    }
-    
-}
-
-// ================================================================================================
-
-
-
-
-// PARSER ========================================================================================
-
-function parser(userInput) {
-
-    userInput = userInput.replace("8", "eight");
-    return userInput;
-
-}
-
-// ================================================================================================
-
-
-// EVALUATE USER INPUT ============================================================================
-
-function evaluateUserInput() {
-
-    var userInput = document.getElementById("userInput").value.toLowerCase();
-
-    // check that user has inputted something
-    if (userInput != "") {
-
-        //parse userInput
-        userInput = parser(userInput);
-        console.log("userInput parsed: " + userInput);
-
-        // split userInput into words
-        var userInputWords = userInput.split(" ");
-
-
-        // iterate through words in userInput
-        for (var i = 0; i < userInputWords.length; i++)
-            // if the word is a key of the keyRoomDict
-            if (currentRoom.keyRoomDict[userInputWords[i]]) {
-                console.log(currentRoom.keyRoomDict[userInputWords[i]]);
-                logArray = [];
-                // if the keyValue is a word
-                if (isNaN(currentRoom.keyRoomDict[userInputWords[i]] * 1)) {
-                    currentRoom = rooms[currentRoom.keyRoomDict[userInputWords[i]]];
-                    console.log("Current Room: " + currentRoom.name);
-                    printText(printRoomInfo());
-                    return;
-                }
-                // if the keyValue is a number
-                else {
-                    currentRoom.statusIndex = currentRoom.keyRoomDict[userInputWords[i]];
-                    currentRoom.playAudio = false;
-                    currentRoom.displayDelayedDescription = false;
-                    printText(printRoomInfo());
-                    return;
-                }
-            }
-
-        // return message when userInput does not contain keyString
-        printText("Nothing happens...");
+    if (currentRoom.displayDelayedDescription) {
+        printToLog(currentRoom.delayedDescription);
     }
 }
 
 // ================================================================================================
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -165,26 +184,21 @@ function evaluateUserInput() {
 
 // ROOM OBJECT =====================================================================================
 
-let Room = function(name, status, exits, keyRoomDict, delayTime, delayedDescription, inputVisible, inputVisibleAfterDelay, audio, audioName, playAudio, statusIndex, displayDelayedDescription) {
+let Room = function(name, descriptions, descriptionIndex, keyRoomDict, inputVisible, inputVisibleAfterDelay, audio, audioName, playAudio, displayDelayedDescription) {
     this.name = name;
-    this.status = status;
-    this.exits = exits;
+    this.descriptions = descriptions;
+    this.descriptionIndex = descriptionIndex;
     this.keyRoomDict = keyRoomDict;
-    this.delayTime = delayTime;
-    this.delayedDescription = delayedDescription;
     this.inputVisible = inputVisible;
     this.inputVisibleAfterDelay = inputVisibleAfterDelay;
     this.audio = audio;
     this.audioName = audioName;
     this.playAudio = playAudio;
-    this.statusIndex = statusIndex;
-    this.displayDelayedDescription = displayDelayedDescription;
-
 };
 
-rooms["firstRoomMiddle"] = new Room("firstRoom", ["It is pitch dark.", "Which way should you move?"], [], { eight: "1", right: "secondRoom" }, 45000, "Do you have the time?", false, true, openingAudio, "opening audio", true, 0, true);
+// last description is the delayed description
 
-
+rooms["firstRoomMiddle"] = new Room("firstRoom", ["It is pitch black.", "Which way should you move?", "Do you have the time?"], 0, { eight: "1", right: "secondRoom" }, false, true, openingAudio, "opening audio", true);
 
 
 // ================================================================================================
